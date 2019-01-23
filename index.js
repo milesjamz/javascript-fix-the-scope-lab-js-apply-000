7b241212cc11a50f1b91d242051f0448acf39772
mjm03 <mjm03@hampshire.edu>
2019-01-23 00:22:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,11 @@ function yourAnimal() {
   return animal[1]
 }
 
-  function add2(n) {
-   var two = 2
-    return n + two;
-  }
-
-  // Feel free to move things around!
-  const two = 25
+ function add2(n) {
+ var two = 2
+  return n + two;
 }
+
 
 var funkyFunction = function() {
   return function() {
